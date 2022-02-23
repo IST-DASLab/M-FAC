8116367fb537b48484e2e4bde24f11f42b117f8a
Eldar Kurtic <eldar.ciki@gmail.com>
2022-02-23 15:52:23 +0100

    Fix for non-default CUDA devices in kernel

--- hinv_cuda.cpp
+++ hinv_cuda.cpp
@@ -1,13 +1,16 @@
 #include <torch/extension.h>
+#include <c10/cuda/CUDAGuard.h>
 
 torch::Tensor hinv_setup_cuda(torch::Tensor tmp, torch::Tensor coef);
 torch::Tensor hinv_mul_cuda(torch::Tensor giHig, torch::Tensor giHix);
 
 torch::Tensor hinv_setup(torch::Tensor tmp, torch::Tensor coef) {
+  const at::cuda::OptionalCUDAGuard device_guard(device_of(tmp));
   return hinv_setup_cuda(tmp, coef);
 }
 
 torch::Tensor hinv_mul(torch::Tensor giHig, torch::Tensor giHix) {
+  const at::cuda::OptionalCUDAGuard device_guard(device_of(giHig));
   return hinv_mul_cuda(giHig, giHix);
 }
 
